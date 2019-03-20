0c3648a70688823dab2a7c26f42afe9ab024974d
faridghafooriadl <faridghafooriadl@gmail.com>
2019-03-21 01:00:19 +0330

    addSkill.js added

--- src/main/javascript/addSkill.js
+++ src/main/javascript/addSkill.js
@@ -1,5 +1,21 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const url = 'http://localhost:8080/projects';
+const url = 'http://localhost:8080/addSkill';
 
-request.open('GET', url, true);
-request.send();
+let response = '';
+const request = new XMLHttpRequest();
+
+request.open('POST', url, true);
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+request.onreadystatechange = function () {
+	if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+		response = request.responseText;
+		getResponse(response);
+	}
+};
+
+function getResponse(res) {
+	console.log(res);
+}
+
+request.send('data={"skillName"="Node.js"}');
