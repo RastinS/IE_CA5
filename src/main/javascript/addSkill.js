2bdbba10018c4404cab0966a73c35b4efe3cc1bb
faridghafooriadl <faridghafooriadl@gmail.com>
2019-03-21 00:37:59 +0330

    fix pom file bug and project.js finished

---
+++ src/main/javascript/addSkill.js
@@ -0,0 +1,5 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const url = 'http://localhost:8080/projects';
+
+request.open('GET', url, true);
+request.send();
--- src/main/javascript/projects.js
+++ src/main/javascript/projects.js
@@ -1,13 +1,18 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const url = 'http://localhost:8080/';
+const url = 'http://localhost:8080/projects';
 
 let response = '';
 const request = new XMLHttpRequest();
 request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 200) {
-        // response = JSON.parse(request.responseText);
-        console.log(request)
-    }
+	if (request.readyState === 4 && request.status === 200) {
+		response = JSON.parse(request.responseText);
+		getResponse(response);
+	}
 };
+
+function getResponse(res) {
+	console.log(res);
+}
+
 request.open('GET', url, true);
 request.send();
